f347ddefce876924b8970181777bf4a0c988e1d8
Dorsa Majdi <dorsa.majdi@gmail.com>
2023-03-12 20:11:47 +0330

    add main

--- scanner.py
+++ scanner.py
@@ -8,12 +8,18 @@ class Scanner:
         self.dfa: dfa_lib.DFA
         self.__create_the_dfa()
         self.current_token: (TokenType, str) = TokenType.ERROR, "not implemented"
-        self.state_types: dict = {}  # {STATE_ID: TOKEN_TYPE} and for keywords also we use ID in this dictionary
+        self.state_dict = {18: 'Unmatched comment', 19: 'SYMBOL', 14: 'SYMBOL', 16: 'Invalid number', 2: 'NUM',
+                           6: 'COMMENT', 15: 'Unclosed comment', 8: 'ID/KW', 10: 'SYMBOL',
+                           11: 'SYMBOL'}  # {STATE_ID: TOKEN_TYPE} and for keywords also we use ID in this dictionary
         self.keywords = {"if": Keyword.IF, "else": Keyword.ELSE, "void": Keyword.VOID, "int": Keyword.INT,
                          "repeat": Keyword.REPEAT, "until": Keyword.UNTIL, "return": Keyword.RETURN}
+        self.special_states = {11, 8, 2, 19}
         self.buffer_size = 1024  # size of each buffer in bytes
         self.buffer = []
         self.file = open(file_name, 'r')
+        self.EOF = False
+        self.EOL = False
+        self.NewLine = False
 
     def get_next_token(self) -> (TokenType, str):
         """
@@ -27,27 +33,46 @@ class Scanner:
                 if not buff:  # stop if end of file is reached
                     self.buffer += '\0'
                 else:
-                    self.buffer += buff
+                    chars = [char for char in buff]
+                    self.buffer += chars
                 current_char = self.buffer.pop(0)
+                if current_char == '\0':
+                    self.EOF = True
+                if current_char == '\n':
+                    self.NewLine = True
+                elif self.NewLine:      # transition point
+                    self.NewLine = False
+                    self.EOL = True
+                else:
+                    self.EOL = False
                 if self.dfa.move(current_char):
                     self.current_token = self.dfa.get_current_token()
-                    if self.dfa.if_special_states(self.current_token[0]):
+                    state = self.current_token[0]
+                    lexeme = self.current_token[1]
+                    if state in self.special_states:
                         self.buffer.insert(0, current_char)
-                    return self.current_token
+                    if state == 8:  # ID/KW
+                        if lexeme in self.keywords:
+                            state = 'KEYWORD'
+                        else:
+                            state = 'ID'
+                    else:
+                        state = self.state_dict.get(state)
+                    return state, lexeme
 
-    def __detect_token_type(self, state_id: int, lexeme: str) -> TokenType:
-        """
-        This method inputs a number as the state_id and detects what token_type does that state
-            specify based on the dfa. It also inputs a string as lexeme which is used to differentiate
-            the types KEYWORD and ID from each other.
-        :param state_id:
-        :param lexeme: It is used to differentiate the types KEYWORD and ID from each other.
-        :return: token_type: TokenType
-        """
-        state_type = self.state_types[state_id]
-        if state_type == TokenType.ID and lexeme in self.keywords.keys():
-            return TokenType.KEYWORD
-        return state_type
+    # def __detect_token_type(self, state_id: int, lexeme: str) -> TokenType:
+    #     """
+    #     This method inputs a number as the state_id and detects what token_type does that state
+    #         specify based on the dfa. It also inputs a string as lexeme which is used to differentiate
+    #         the types KEYWORD and ID from each other.
+    #     :param state_id:
+    #     :param lexeme: It is used to differentiate the types KEYWORD and ID from each other.
+    #     :return: token_type: TokenType
+    #     """
+    #     state_type = self.state_types[state_id]
+    #     if state_type == TokenType.ID and lexeme in self.keywords.keys():
+    #         return TokenType.KEYWORD
+    #     return state_type
 
     def __create_the_dfa(self):
         """
@@ -59,12 +84,3 @@ class Scanner:
         dfa = dfa_lib.DFA()
         # TODO: to create the dfa object and define the states and transitions
         self.dfa = dfa
-
-    def tokenize_file(self, file_name, symbol_table):
-        with open(file_name, 'r') as f:  # open file for binary reading mode
-            while True:
-                buffer = f.read(self.buffer_size)  # read buffer_size bytes from file
-                if not buffer:  # stop if end of file is reached
-                    break
-                chars = [char for char in buffer]  # separate buffer character by character
-                for char in chars:
