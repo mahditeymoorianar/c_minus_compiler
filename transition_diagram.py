287a991a5c92a567b1f940a1f6ed012daaa89e46
Dorsa Majdi <dorsa.majdi@gmail.com>
2023-04-17 12:36:45 +0330

    add first and symbol

--- transition_diagram.py
+++ transition_diagram.py
@@ -2,8 +2,13 @@ import scanner
 
 # TODO: Handle $ + Add panic mode + First and follow to detect errors early + tree
 
-terminals = []
-non_terminals = ['Program', ' Declaration-list',]   # This is line 1 and so on...
+
+class Symbol:
+    def __init__(self, name, terminal, first):
+        self.name = name
+        self.terminal = terminal
+        self.first = first
+# eps is also a symbol name
 
 class Transition_Diagram:
     def __init__(self, name):
@@ -11,13 +16,13 @@ class Transition_Diagram:
         self.states = {}
         """" Define the states and transitions with their associated tokens
         note to first add the terminal tokens then non-terminal then epsilon
-        I assume this is the order we need to check
         Indicate final state with 'FINAL'
+        Ti is a Symbol object
         states = {
             "S": {
-                "T1": "S",
-                "T2": "S2",
-                "T3": "S2"
+                T1: "S",
+                T2: "S2",
+                T3: "S2"
             },
             "S2": {}
         }
@@ -26,33 +31,29 @@ class Transition_Diagram:
         self.current_token = None
         self.traversed_edge = None
 
-    def add_state (self, begin_state, edge_token, end_state):
+    def add_state(self, begin_state, edge_token, end_state):
         if begin_state not in self.states:
             self.states[begin_state] = {}
         self.states[begin_state][edge_token] = end_state
 
     def transition(self):
-        for transition_token in self.states[self.current_state]:
-            if transition_token in terminals:
-                if transition_token == self.current_token:
-                    self.current_state = self.states[self.current_state][transition_token]
-                    self.traversed_edge = 'TERMINAL'
-                    return
-            else:
-                self.current_state = self.states[self.current_state][transition_token]
-                self.traversed_edge = transition_token
-                return
+        for transition_symbol in self.states[self.current_state]:
+            if self.current_token in transition_symbol.first:
+                self.current_state = self.states[self.current_state][transition_symbol]
+                self.traversed_edge = transition_symbol
+                return True
+        return False
 
 
 class Parser:
     def __init__(self, scanner_module):
         self.scanner = scanner_module
-        self.transition_diagrams = {}       # {name of diagram : object}
+        self.transition_diagrams = {}  # {name of diagram : object}
         self.diagram_stack = []
         self.current_diagram = None
 
     def run(self):
-        self.current_diagram = self.transition_diagrams['Program']      # the name of the start state
+        self.current_diagram = self.transition_diagrams['Program']  # the name of the start state
         self.current_diagram.current_token = self.scanner.get_next_token()
         self.diagram_stack.append(self.current_diagram)
         while True:
@@ -60,7 +61,7 @@ class Parser:
             self.current_diagram.transition()
             if not self.current_diagram.current_state == 'FINAL':
                 edge = self.current_diagram.traversed_edge
-                if not edge == 'TERMINAL':
+                if not edge.terminal:
                     self.diagram_stack.append(self.transition_diagrams[edge])
                 else:
                     self.current_diagram.current_token = self.scanner.get_next_token()
@@ -73,3 +74,4 @@ parser = Parser(None)
 d = Transition_Diagram('Program')
 d.add_state('1', ' Declaration-list', 'FINAL')
 parser.transition_diagrams['Program'] = d
+symbols = []
